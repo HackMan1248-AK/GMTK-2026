f6378af940efa055af5f98dac7b2861dc14a3237
HackMan1248-AK <aadikhose23@gmail.com>
2026-07-23 20:09:20 +0530

    Locking, and Timer mechanism

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,7 @@ var can_flash_light = true
 var flashlight_on = false
 
 @export var speed : float = 300.0
+@export var movement_locked : float = false
 var interactable = null
 
 @onready var ingredient_point: Marker2D = $Ingredient
@@ -32,6 +33,11 @@ func _unhandled_input(event):
 			can_flash_light = false
 
 func _physics_process(_delta: float):
+	if movement_locked:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+	
 	flash_light.look_at(get_global_mouse_position())
 
 	if flashlight_on:
@@ -59,6 +65,11 @@ func pickup(item: Node2D):
 	# Snap into place
 	item.position = Vector2.ZERO
 	item.rotation = 0
+	
+func lock_movement(duration: float):
+	movement_locked = true
+	await get_tree().create_timer(duration).timeout
+	movement_locked = false
 
 func _on_active_timer_timeout() -> void:
 	flashlight_on = false
--- scripts/station.gd
+++ scripts/station.gd
@@ -1,7 +1,15 @@
 extends Interactable
 @onready var player: CharacterBody2D = $"../../Player"
 
+@onready var slicing_timer: Timer = $Timers/SlicingTimer
+@onready var assembling_timer: Timer = $Timers/AssemblingTimer
+@onready var baking_timer: Timer = $Timers/BakingTimer
+@onready var grilling_timer: Timer = $Timers/GrillingTimer
+@onready var freezing_timer: Timer = $Timers/FreezingTimer
+
 var held_item = null
+var can_pickup = false
+var active_timer : Timer
 
 enum Station {
 	FRIDGE,
@@ -13,44 +21,40 @@ enum Station {
 }
 
 @export var station: Station
+@export var should_lock: bool
 
 func interact() -> void:
 	if player.held_item != null:
 		held_item = player.held_item
+		held_item.reparent(self)
+		
+		held_item.position = Vector2.ZERO
+		held_item.rotation = 0
+		player.held_item = null
+		
 		match station:
 			Station.FRIDGE:
-				held_item.reparent(self)
-				
-				held_item.position = Vector2.ZERO
-				held_item.rotation = 0
-				print("Freezing")
+				freezing_timer.start()
+				active_timer = freezing_timer
 			Station.PANTRY:
-				held_item.reparent(self)
-				
-				held_item.position = Vector2.ZERO
-				held_item.rotation = 0
 				print("Storing")
 			Station.ASSEMBLY:
-				held_item.reparent(self)
-				
-				held_item.position = Vector2.ZERO
-				held_item.rotation = 0
-				print("Assembling")
+				assembling_timer.start()
+				active_timer = assembling_timer
 			Station.BAKING:
-				held_item.reparent(self)
-				
-				held_item.position = Vector2.ZERO
-				held_item.rotation = 0
-				print("Baking")
+				baking_timer.start()
+				active_timer = baking_timer
 			Station.GRILLING:
-				held_item.reparent(self)
-				
-				held_item.position = Vector2.ZERO
-				held_item.rotation = 0
-				print("Grilling")
+				grilling_timer.start()
+				active_timer = grilling_timer
 			Station.SLICING:
-				held_item.reparent(self)
+				slicing_timer.start()
+				active_timer = slicing_timer
 				
-				held_item.position = Vector2.ZERO
-				held_item.rotation = 0
-				print("Slicing")
+		if should_lock:
+			print(active_timer.time_left)
+			player.lock_movement(active_timer.time_left)
+	else:
+		if held_item != null:
+			player.pickup(held_item)
+			held_item = null
